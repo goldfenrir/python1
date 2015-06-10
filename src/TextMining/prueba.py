5f60818c36b105e976e21b712460b2c4acff8b4a
goldfenrir <jdbustamante@pucp.pe>
2015-06-10 00:15:16 -0500

    Merge master into HEAD
    Conflicts:
    	Sistemas/src/sistemas/MantenerObjetivos.java

--- src/TextMining/prueba.py
+++ src/TextMining/prueba.py
@@ -10,7 +10,48 @@ from nltk.corpus import stopwords
 
 import math
 import string
+#definiciones
 
+def quitarComilla(campo):  #quitar comas dentro de un campo con comillas
+    flagComilla=0
+    campoS=campo.strip() #campo sin espacios
+    if (campoS[0]=="\"" and campoS[len(campoS)-1]=="\""):
+        campoS=campoS[1:-1]
+        campoS = campoS.translate(None, ',\"')        
+        return campoS
+    else:
+        return -1
+    return -1
+
+def limpiarLinea(linea): #se limpia una linea
+    linea=linea.strip() #campo sin espacios
+    #linea = linea sin acentos
+    #linea = linea sin puntuacino menos comillas y comas
+    for x in range(0,len(linea)-1):
+        if (linea[x]=="," and linea[x+1]!="\""):
+            
+    
+    return linea
+
+
+
+
+
+#Lectura y limpieza
+#Quitamos todos los caracteres de puntuacion
+
+
+#fin lectura y limpieza
+
+
+
+
+
+
+
+
+
+"""
 archLectura=open('TA_Registros_etiquetados1.csv','r')#Leer archivo
 
 #archBag=open('Bag of words.txt','w')#Archivo de salida
@@ -45,3 +86,4 @@ for word in words:
 print "Nuevo: %s " % (comentarios)
 
 #print stopwords.words('spanish')
+"""
